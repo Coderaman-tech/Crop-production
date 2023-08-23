66be5e16f8462b832feae380b41ce1cdbfcfb440
coderaman_tech <amankumarshrivastava835@gmail.com>
2023-08-23 22:54:20 +0530

    update

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route('/')
-
-def home():
-    return "Hello World"
+from flask import Flask,request,jsonify
+import pickle
+import numpy as np
 
 
-@app.route('/predict',methods=['POST'])
-
-def predict():
-    N=request.form.get('N')
-    # P=request.form.get('P')
-    # K=request.form.get('K')
-    # temperature=request.form.get("temperature")
-    # ph=request.form.get('ph')
-    rainfall=request.form.get('rainfall')
-    result={'N':N,'rainfall':rainfall}
-
-    return jsonify(result)
 
 label_mapping={20: 'rice',
  11: 'maize',
@@ -43,6 +26,34 @@ label_mapping={20: 'rice',
  6: 'cotton',
  8: 'jute',
  5: 'coffee'}
+
+
+model=  pickle.load(open('model.pkl','rb'))
+app=Flask(__name__)
+
+
+@app.route('/')
+
+def home():
+    return "Hello World"
+
+
+@app.route('/predict',methods=['POST'])
+
+
+def predict():
+    N=request.form.get('N')
+    P=request.form.get('P')
+    K=request.form.get('K')
+    temperature=request.form.get("temperature")
+    humidity=request.form.get("humidity")
+    ph=request.form.get('ph')
+    rainfall=request.form.get('rainfall')
+    input_query=np.array([[N,P,K,temperature,humidity,ph,rainfall]])
+    result=model.predict(input_query)[0]
+    return jsonify({'crop':label_mapping[result]})
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
